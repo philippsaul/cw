896354304fd004e3ed17a50fc7058f5e6c3221a1
philippsaul <philipp@psaul.de>
2021-07-14 23:04:36 +0200

    ps4 änderungen in main.py damit ps4 tut

--- src/rtsp/ps4controller.py
+++ src/rtsp/ps4controller.py
@@ -2,10 +2,9 @@ from ps4test import Controller
 import threading
 
 ps4 = Controller(interface="/dev/input/js0", connecting_using_ds4drv=False)
-# you can start listening before controller is paired, as long as you pair it within the timeout window
-
 ps4_thread = threading.Thread(target=ps4.listen, name="test")
 ps4_thread.start()
 
 while True:
-    print(ps4.xb)
+    # print(ps4.rt)
+    print((ps4.lt + 32767)/65534)
