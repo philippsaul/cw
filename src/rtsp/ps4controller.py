c204c253d1cb69b2538a96e32e75b3fb6cbdd473
philippsaul <philipp@psaul.de>
2021-07-14 22:51:32 +0200

    ps4controller optimiert

--- src/rtsp/ps4controller.py
+++ src/rtsp/ps4controller.py
@@ -1,22 +1,11 @@
-# from ps4test import Controller
 from ps4test import Controller
 import threading
-import time
 
-class MyController(Controller):
-
-    def __init__(self, **kwargs):
-        Controller.__init__(self, **kwargs)
-
-
-controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=False)
+ps4 = Controller(interface="/dev/input/js0", connecting_using_ds4drv=False)
 # you can start listening before controller is paired, as long as you pair it within the timeout window
 
-download_thread = threading.Thread(target=controller.listen, name="test")
-download_thread.start()
+ps4_thread = threading.Thread(target=ps4.listen, name="test")
+ps4_thread.start()
 
-# controller.listen(timeout=60)
 while True:
-    print(controller.xb)
-    # time.sleep(0.1)
-
+    print(ps4.xb)
