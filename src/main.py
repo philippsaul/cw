f21b07cff54e937405b67a89d54acbbcc6ceeb5a
larshebing <larshebing2007@gmail.com>
2021-07-16 18:56:19 +0200

    blabla

--- src/main.py
+++ src/main.py
@@ -3,19 +3,19 @@ from motor.motors import Motors
 from video import web_streaming
 from video.camera import Camera
 import threading
-import time
 
 myGamepad = Gamepad()
 myMopets = Motors()
 
 myCam = Camera()
 
-stream_thread = threading.Thread(target=web_streaming.start, name="test")
+#start stram in background
+stream_thread = threading.Thread(target=web_streaming.start)
 stream_thread.start()
 
 while True:
      frame = myCam.getFrame()
+     
      web_streaming.video_frame = frame
      myGamepad.get_data()
-
      myMopets.gamepadcontroll(myGamepad.lt, myGamepad.rt, myGamepad.ls, myGamepad.ab, myGamepad.bb, myGamepad.xb, myGamepad.yb)
