ddc16d566a0fe3845c0ccbff849017a1e85d723a
larshebing <larshebing2007@gmail.com>
2021-07-17 23:44:03 +0200

    added ai for linedetection

--- src/linedetection/processdata.py
+++ src/linedetection/processdata.py
@@ -10,45 +10,40 @@ def picdata():
     x, new_ls = np.loadtxt("./data/textdatei.txt", skiprows=0, unpack=True)
     for element in range(len((new_ls))):
         if(float(new_ls[element]) < 0.05 and float(new_ls[element]) >= -0.05):
-            new_ls[element] = int(5)
-        elif(float(new_ls[element]) >= 0.05 and float(new_ls[element]) < 0.2):
-            new_ls[element] = int(6)
-        elif(float(new_ls[element]) >= 0.2 and float(new_ls[element]) < 0.4):
-            new_ls[element] = int(7)
-        elif(float(new_ls[element]) >= 0.4 and float(new_ls[element]) < 0.6):
-            new_ls[element] = int(8)
-        elif(float(new_ls[element]) >= 0.6 and float(new_ls[element]) < 0.8):
-            new_ls[element] = int(9)
-        elif(float(new_ls[element]) >= 0.8 and float(new_ls[element]) <= 1.0):
-            new_ls[element] = int(10)
-        elif(float(new_ls[element]) >= -0.2 and float(new_ls[element]) < -0.05):
-            new_ls[element] = int(4)
-        elif(float(new_ls[element]) >= -0.4 and float(new_ls[element]) < -0.2):
-            new_ls[element] = int(3)
-        elif(float(new_ls[element]) >= -0.6 and float(new_ls[element]) < -0.4):
-            new_ls[element] = int(2)
-        elif(float(new_ls[element]) >= -0.8 and float(new_ls[element]) < -0.6):
-            new_ls[element] = int(1)
-        elif(float(new_ls[element]) >= -1.0 and float(new_ls[element]) < -0.8):
             new_ls[element] = int(0)
+        elif(float(new_ls[element]) >= -0.1 and float(new_ls[element]) < -0.05):
+            new_ls[element] = int(1)
+        elif(float(new_ls[element]) >= -0.15 and float(new_ls[element]) < -0.1):
+            new_ls[element] = int(2)
+        elif(float(new_ls[element]) >= -0.2 and float(new_ls[element]) < -0.15):
+            new_ls[element] = int(3)
+        elif(float(new_ls[element]) >= -0.25 and float(new_ls[element]) < -0.2):
+            new_ls[element] = int(4)
+        elif(float(new_ls[element]) >= -0.3 and float(new_ls[element]) <= -1.0):
+            new_ls[element] = int(5)
     # print(len(new_ls))
     for j in range(len(new_ls)):
         # print("picdata")
-        print(j)
+        # print(j)
         new_Array.append(shrinkpic(cv2.imread("./data/pic_" + str(j) + ".jpg")))
     return new_Array, new_ls
 
 def shrinkpic(pic_Array):
     temp_Array = []
-    for zeile in range(len(pic_Array)):
-        # print(pic_Array)
+    for zeile in range(400): 
+        temp = zeile + 215
+        # print(temp)
+        # print(len(pic_Array))
         # print(zeile)
         temp_Array.append([])
-        for pixel in range(len(pic_Array[zeile])):
-            # print(pixel)
-            temp_Array[zeile].append(pic_Array[zeile][pixel][0])
+        for pixel in range(len(pic_Array[temp])):
+            # print(pic_Array[temp][pixel][0])
+            temp_Array[zeile].append(pic_Array[temp][pixel][0])
+    
+    # print(temp_Array)
     return temp_Array
 
-a, b = picdata()
+a,b = picdata()
 # print(a[1])
-print(b)
+# print(len(a))
+
---
+++ src/linedetection/train.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+from tensorflow import keras
+import processdata
+import numpy as np
+
+x_train, y_train = processdata.picdata()
+x_val = x_train[108:30]
+x_train = x_train[0:108]
+
+y_val = y_train[108:30]
+y_train = y_train[0:108]
+
+# (x_train, y_train), (x_val, y_val) = keras.datasets.fashion_mnist.load_data()
+# print(x_val[0])
+def preprocess(x, y):
+  x = tf.cast(x, tf.float32) / 255.0
+  y = tf.cast(y, tf.int64)
+
+  return x, y
+
+def create_dataset(xs, ys, n_classes=6):
+  ys = tf.one_hot(ys, depth=n_classes)
+  return tf.data.Dataset.from_tensor_slices((xs, ys)) \
+    .map(preprocess) \
+    .shuffle(len(ys)) \
+    .batch(18)
+
+train_dataset = create_dataset(x_train, y_train)
+val_dataset = create_dataset(x_val, y_val)
+
+model = keras.Sequential([
+    keras.layers.Reshape(target_shape=(960 * 400,), input_shape=(960, 400)),
+    keras.layers.Dense(units=256, activation='relu'),
+    keras.layers.Dense(units=192, activation='relu'),
+    keras.layers.Dense(units=128, activation='relu'),
+    keras.layers.Dense(units=6, activation='softmax')
+])
+
+model.compile(optimizer='adam', 
+              loss=tf.losses.CategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(
+    train_dataset.repeat(), 
+    epochs=10, 
+    steps_per_epoch=500,
+    validation_data=val_dataset.repeat(), 
+    validation_steps=2
+)
+
+predictions = model.predict(val_dataset)
+
+print(np.argmax(predictions[0]))
+
+
