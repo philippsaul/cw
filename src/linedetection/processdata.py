1728972dca9ec1e9ea3e63ba87b4f69c34bada51
philippsaul <philipp@psaul.de>
2021-07-24 15:22:20 +0200

    .

--- src/linedetection/processdata.py
+++ src/linedetection/processdata.py
@@ -23,29 +23,14 @@ def picdata():
             new_ls[element] = int(5)
     print(len(new_ls))
     for j in range(len(new_ls)):
-        # print("picdata")
-        # print(j)
         new_Array.append(shrinkpic(cv2.imread("./data/pic_" + str(j) + ".jpg")))
-    print("len new array")
-    print(len(new_Array))
     return new_Array, new_ls
 
 def shrinkpic(pic_Array):
     temp_Array = []
     for zeile in range(400): 
         temp = zeile + 215
-        # print(temp)
-        # print(len(pic_Array))
-        # print(zeile)
         temp_Array.append([])
         for pixel in range(len(pic_Array[temp])):
-            # print(pic_Array[temp][pixel][0])
             temp_Array[zeile].append(pic_Array[temp][pixel][0])
-    
-    # print(temp_Array)
     return temp_Array
-
-# a,b = picdata()
-# print(a[1])
-# print(len(a))
-
--- src/linedetection/train.py
+++ src/linedetection/train.py
@@ -5,21 +5,18 @@ import numpy as np
 
 x_train, y_train = processdata.picdata()
 
-x_val = x_train[108:30]
-x_train = x_train[0:108]
+x_val = x_train[4:7]
+x_train = x_train[0:4]
 
-y_val = y_train[108:30]
-y_train = y_train[0:108]
-print("shape of x train:")
-print(np.shape(x_train))
-print("shape of y train:")
-print(np.shape(y_train))
+y_val = y_train[4:7]
+y_train = y_train[0:4]
 
-y_train = np.asarray(y_train).astype('int8').reshape((-1,1))
-y_val = np.asarray(y_val).astype('int8').reshape((-1,1))
+# y_train = np.asarray(y_train).astype('int8').reshape((-1,1))
+# y_train = np.asarray(x_val).astype('int8').reshape((-1,1))
+# y_val = np.asarray(y_val).astype('int8').reshape((-1,1))
 
 # (x_train, y_train), (x_val, y_val) = keras.datasets.fashion_mnist.load_data()
-# print(x_val[0])
+
 def preprocess(x, y):
   x = tf.cast(x, tf.float32) / 255.0
   y = tf.cast(y, tf.int64)
@@ -37,23 +34,28 @@ print("shape of x train:")
 print(np.shape(x_train))
 print("shape of y train:")
 print(np.shape(y_train))
+print("shape of x test:")
+print(np.shape(x_val))
+print("shape of y test:")
+print(np.shape(y_val))
+
 train_dataset = create_dataset(x_train, y_train)
 val_dataset = create_dataset(x_val, y_val)
 
-print("Traindataset")
-print(train_dataset)
-print(np.shape(train_dataset))
+# print("Traindataset")
+# print(train_dataset)
+# print(np.shape(train_dataset))
 
 model = keras.Sequential([
-    keras.layers.Reshape((960, 400), input_shape=(400,960)),
+    keras.layers.Reshape((960, 400), input_shape=(960,400)),
     keras.layers.Dense(units=256, activation='relu'),
     keras.layers.Dense(units=192, activation='relu'),
     keras.layers.Dense(units=128, activation='relu'),
-    kera  .layers.Dense(units=6, activation='softmax')
+    keras.layers.Dense(units=6, activation='softmax')
 ])
 
 model.compile(optimizer='adam', 
-              loss=tf.losses.CategoricalCrossentropy(from_logits=True),
+              loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
 history = model.fit(
