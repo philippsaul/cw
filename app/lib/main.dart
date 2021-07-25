39b422b2f96555dc97f9a36c2ac466f1e61178ff
Philipp Saul <saul-p@t-online.de>
2021-07-25 13:45:44 +0200

    controller working

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:control_pad/control_pad.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('JetBot Controller'),
+            // appBar: AppBar(
+            //   title: Text('JetBot Controller'),
+            // ),
+            body: Container(
+      color: Colors.white,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 100),
+            child: JoystickView(
+              opacity: 0.2,
+              showArrows: false,
+              onDirectionChanged: (degrees, distance) => print(degrees),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 100),
+            child: JoystickView(
+              opacity: 0.2,
+              showArrows: false,
+              onDirectionChanged: (degrees, distance) => print(distance),
+            ),
+          )
+        ],
       ),
-      body: Container(
-        color: Colors.white,
-        child: JoystickView(
-          opacity: 0.5,
-          onDirectionChanged: (degrees, distance) => print(degrees),
-        ),
-      ),
-    ));
+    )));
   }
 }
 
 void main() {
-  runApp(HomePage());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft])
+      .then((_) {
+    runApp(HomePage());
+  });
 }
